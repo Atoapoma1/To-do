42c774be60b8bab7b5527061d66dcca2ae57ccd4
Atoapoma1 <otafinancialservice@gmail.com>
2024-03-26 13:58:18 +0000

    Updated changes

--- todo.py
+++ todo.py
@@ -3,50 +3,74 @@
 
 filepath = r"C:/Users/Ato/Desktop/COURSES/PYTHON PROJECTS/WEBSERVER1/To do app/todos.txt"
 while True:
-    user_action = input("Type something; add, show, edit, complete or exit")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case "add":
-            todo = input("Enter a todo: ") + "\n"
+    if user_action.startswith("add"):
+        todo = user_action[4:]
 
-            file = open(filepath, mode="r")
+        with open(filepath, mode="r") as file:
             todos = file.readlines()
-            file.close()
 
-            todos.append(todo)
+        todos.append(todo + "\n")
 
-            file = open(filepath, mode="w")
+        with  open(filepath, mode="w") as file:
             file.writelines(todos)
-            file.close()
 
-        case "show" | "view":
-            file = open(filepath, mode="r")
+    elif user_action.startswith("show"):
+        with open(filepath, mode="r") as file:
             todos = file.readlines()
-            file.close()
 
-            # new_todos = []
+        # new_todos = []
 
-            # for item in todos:
-            #     new_item = item.strip("\n")
-            #     new_todos.append(new_item)
+        # for item in todos:
+        #     new_item = item.strip("\n")
+        #     new_todos.append(new_item)
 
-            #Using list comprehsion
-            new_todos = [item.strip("\n") for item in todos]
+        #Using list comprehsion
+        # new_todos = [item.strip("\n") for item in todos]
 
-            for index, item in enumerate(new_todos):
-                print(f"{index + 1} - {item}")
+        for index, item in enumerate(todos):
+            item = item.strip("\n")
+            print(f"{index + 1} - {item}")
 
-        case "edit":
-            number = int(input("Type the number of todo to edit"))
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
             number = number - 1
-            new_todo = input("Enter a new todo")
-            todos[number] = new_todo
-        case "complete":
-            number = int(input("Number of todos to complete"))
-            todos.pop(1 - 1)
-        case "exit":
-            break
+
+            with open(filepath, mode="r") as file:
+                todos = file.readlines()
+
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo + "\n"
+            
+            with open(filepath, mode="w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid")
+
+    elif user_action.startswith("complete"):
+        number = int(user_action[9:])
+
+        with open(filepath, mode="r") as file:
+            todos = file.readlines()
+
+        index = number - 1
+        todo_to_remove = todos[index].strip("\n")
+        todos.pop(index)
+
+        with open(filepath, mode="w") as file:
+            file.writelines(todos)
+
+        message = f"Todo: {todo_to_remove} was removed from the list"
+        print(message)
+
+    elif user_action.startswith("exit"):
+        break
+
+    else:
+        print("Command not valid")
         
 print("bye")
  
